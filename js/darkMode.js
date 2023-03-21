22aa570c4a90ccda6868d886135fc5a13c6d2cb9
Uanderson Costa <94417952+uandersoncosta@users.noreply.github.com>
2023-03-21 14:18:49 -0300

    Update darkMode.js

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,13 +1,6 @@
 const bsIcon = document.querySelector("#toggleMode");
 const body = document.body;
 
-// function darkMode() {
-//   this.classList.toggle("bi-brightness-high");
-//   if(this.classList.toggle("bi-moon-fill")){
-//     body.classList.toggle("dark")
-//   }
-// }
-
 bsIcon.addEventListener("click", () => {
   if (body.classList.contains("light")) {
     bsIcon.classList.replace('bi-moon-fill', "bi-brightness-high-fill")
